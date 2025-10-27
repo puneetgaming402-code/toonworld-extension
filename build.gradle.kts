6a40156902d0c5775155657d41ddd3319eb51c56
puneetgaming402-code <puneetgaming402@gmail.com>
2025-10-27 14:07:33 +0530

    build.gradle.kts
    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.lagradost.cloudstream3.gradle") version "1.0"
+}
+
+cloudstream {
+    description = "ToonWorldForAll extension for CloudStream"
+    authors = listOf("Puneet")
+    language = "en"
+    status = 1
+}
